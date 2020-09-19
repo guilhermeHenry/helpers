6eba644bd5148a9d76edd6ec34db29a722139bc1
guilhermehenryk <gui2037@live.com>
2020-09-18 21:06:17 -0300

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+const rwd = require('app-rwd');
+const collectionsMap = require('collections-map');
+const encapsulation = require('encapsulation-props');
+
+module.exports = {rwd, collectionsMap, encapsulation};
